239369be901af68598233d605843704eb709a5de
paulogbx <paulogabrielbarros4@gmail.com>
2023-11-30 12:22:38 -0300

    commit para o deley

--- components/Detalhes.js
+++ components/Detalhes.js
@@ -1,11 +1,9 @@
-// Importe as bibliotecas necessárias
 import React from 'react';
 import { View, Text } from 'react-native';
 
-// Crie a tela de detalhes
 const DetalhesScreen = ({ route }) => {
   const { id, tituloAtual, descricaoAtual } = route.params;
-
+  
   return (
     <View>
       <Text>Título: {tituloAtual}</Text>
